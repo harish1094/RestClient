4887ff0df9e9690b72d46f6bc3132909fc13cacf
Harish Acharya <44230339+harish1094@users.noreply.github.com>
2022-10-12 11:38:12 +0530

    Update env.js

--- env.js
+++ env.js
@@ -1,4 +1,4 @@
 window._env_ = {
-    REACT_APP_BASE_URL_DEV: "http://127.0.0.1:8000/",
-    REACT_APP_BASE_URL_PROD: "http://127.0.0.1:8000/"
+    REACT_APP_BASE_URL_DEV:"http://127.0.0.1:8000/",
+    REACT_APP_BASE_URL_PROD:"http://127.0.0.1:8000/"
 }
